91b07d23f5f5752fcf6fddfd01252f55aaffbdc5
JunHyun <moyanada@gmail.com>
2022-11-07 20:30:10 +0900

    feat : db user 추가 및 권한

--- docker/mysql/init.d/create_table.sql
+++ docker/mysql/init.d/create_table.sql
@@ -1,3 +1,7 @@
+CREATE USER 'grnmaster'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON *.* TO 'grnmaster'@'%';
+flush privileges;
+
 CREATE TABLE test_table (
     test_name varchar(25) not null,
     test_datetime datetime default NOW() not null
